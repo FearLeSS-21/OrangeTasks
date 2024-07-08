90a6833797248b6cf7ae3fdf58a612004f9f777a
Zeyad Elghadban <127014710+FearLeSS-21@users.noreply.github.com>
2024-07-08 17:59:32 +0300

    Edited Files

--- Product.java
+++ Product.java
@@ -8,12 +8,14 @@ public class Product {
 
         Scanner scanner = new Scanner(System.in);
 
+        /*
+
         //Empty constructor with basic Vars.
-        ProductData x = new ProductData();
+        ProductData x = new ProductData() {};
         x.Print();
 
         //constructor add here by me .
-        ProductData x1 = new ProductData(1200, "bata");
+        ProductData x1 = new ProductData(1200, "bata") {};
         System.out.print("Before Setter and Getter ->  ");
         x1.Print();
 
@@ -32,7 +34,53 @@ public class Product {
 
         ProductData x2 = new ProductData(price, name);
         x2.Print();
+        
+        
+        ProductData2 x4 = new ProductData2(120,"batats",20);
+        
+        x4.print();
+        
+         
+        System.out.println("Enter the type of product (normal, digital):");
+        String productType = scanner.nextLine().trim().toLowerCase();
 
+        ProductInterface product;
+
+        switch (productType) {
+            case "normal": {
+                
+                System.out.println("Price of the product:");
+                int cost = scanner.nextInt();
+                scanner.nextLine();
+
+                System.out.println("Name Of Product:");
+                String name = scanner.next();
+
+                System.out.println(" weight of the product:");
+                int weight = scanner.nextInt();
+
+                product = new ProductData2(cost, name, weight);
+                break;
+            }
+            case "digital": {
+                System.out.println("Enter the cost of the digital product:");
+                int cost = scanner.nextInt();
+                product = new DigitalProduct(cost);
+                break;
+            }
+            default:
+                System.out.println("Invalid product type");
+                return;
+        }
+
+        product.print();
+            */
+        
+        
+        
+        
+        
     }
+
 
 }
--- ProductData.java
+++ ProductData.java
@@ -1,10 +1,36 @@
 package product;
 
+interface ProductInterface {
+
+    void getCost();
+
+    void setCost(int cost);
+
+    void getName();
+
+    void setName(String name);
+
+    void getWeight();
+
+    void setWeight(int weight);
+
+    void print();
+}
+
 public class ProductData {
 
     int cost;
+    int weight;
     String name;
     static int count = 0;
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
 
     public int getCost() {
         return cost;
@@ -43,10 +69,63 @@ public class ProductData {
 
     }
 
-    public String toString() {
+}
 
-        return cost + name;
+class ProductData2 implements ProductInterface {
+
+    private int cost;
+    private int weight;
+    private String name;
+
+    @Override
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public void getWeight() {
+        System.out.println("Weight: " + weight);
+    }
+
+    @Override
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    @Override
+    public void getCost() {
+        System.out.println("Cost: " + cost);
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void getName() {
+        System.out.println("Name: " + name);
+    }
+
+    @Override
+    public void print() {
+        System.out.println("Cost: " + cost + ", Weight: " + weight + ", Name: " + name);
+    }
+
+    public ProductData2(int cost, String name, int weight) {
+
+        this.name = name;
+        this.weight = weight;
+        this.cost = cost;
 
     }
 
 }
+
+
+
+
+
+
+
+
